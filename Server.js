cd81904bbddabf8c51022c33e1ab4a8300183b17
Seif Waleed Mahdy <sm2100383@tkh.edu.eg>
2025-05-01 05:23:58 +0300

    port update

--- Server.js
+++ Server.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const db = require('./Database');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 // Middleware
 app.use(cors()); // Allow Unity to access API
